3800c18b3bf338f5c6670df4d051632c948a29c7
Vasjan <vasjancupri04@gmail.com>
2022-11-14 19:53:39 +0100

    Solved the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  if (!word) {
+    return "";
+  } else {
+    let letterArray = word.split("");
+    return letterArray.reverse().join("");
+  }
 };
 
 // Do not edit below this line
