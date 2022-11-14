e56542f3b01f805104b880c6c92ffb1219d3e3c1
Vasjan <vasjancupri04@gmail.com>
2022-11-14 19:47:03 +0100

    Learned how to use .skip

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (phrase, repetition) {
+  if (repetition <= -1) {
+    return "ERROR";
+  } else {
+    return phrase.repeat(repetition);
+  }
 };
 
 // Do not edit below this line
