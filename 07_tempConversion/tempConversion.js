d84c9cfccc55f569ff7faf1a43f2a4cb61ef0b2d
Vasjan <vasjancupri04@gmail.com>
2022-11-15 19:49:52 +0100

    Finished ex n 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  return Math.round((((temp - 32) * 5) / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  return Math.round(((temp * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
