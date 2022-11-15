e939cce1eea22c18bf00f51b6b25d2c128dabf62
Vasjan <vasjancupri04@gmail.com>
2022-11-15 19:30:22 +0100

    Solved the 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (starting, ending) {
+  if (!Number.isFinite(starting) || !Number.isFinite(ending)) {
+    return "ERROR";
+  } else if (starting < 0 || ending < 0) {
+    return "ERROR";
+  } else {
+    if (starting > ending) {
+      return ((starting / ending) * (starting + ending)) / 2;
+    } else {
+      return ((ending / starting) * (starting + ending)) / 2;
+    }
+  }
 };
 
 // Do not edit below this line
